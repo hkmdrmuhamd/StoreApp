463693a5e42ec9b51a84af2df6804c6bf052c965
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-08-09 16:21:04 +0300

    Categories ViewComponent

---
+++ StoreApp/StoreApp.Web/Components/CategoriesListViewComponent.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using StoreApp.Data.Abstract;
+
+namespace StoreApp.Web.Components;
+
+public class CategoriesListViewComponent : ViewComponent
+{
+    private readonly IStoreRepository _storeRepository;
+
+    public CategoriesListViewComponent(IStoreRepository storeRepository)
+    {
+        _storeRepository = storeRepository;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        return View(_storeRepository
+                    .Products
+                    .Select(c => c.Category)
+                    .Distinct() //Birden fazla aynı kategorideki ürünlerden sadece bir tanesini almaya yarar.
+                    .OrderBy(c => c) //Gelen kayıtları isme göre sıralar.
+                    );
+    }
+}
